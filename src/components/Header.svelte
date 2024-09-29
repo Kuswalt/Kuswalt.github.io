35b10ad459858266bf38f363e8fc24793525e988
Chris Jen Ian D. Roa <chrisianroa@gmail.com>
2024-09-29 14:30:49 +0800

    initial commit

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,8 +3,8 @@
     export let innerHeight;
 
     export let tabs = [
-        { name: "Projects ", link: "#projects" },
         { name: "About me", link: "#about" },
+        { name: "Projects ", link: "#projects" },
         // { name: "Contact", link: "" },
         // {name: '', link: ''},
     ];
@@ -13,7 +13,7 @@
 <header
     class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
         (y > 0
-            ? " py-4 bg-slate-950 border-violet-700"
+            ? " py-4 bg-black border-blue-700"
             : " py-6 bg-transparent border-transparent")}
 >
     <h1 class="font-medium">
@@ -23,15 +23,15 @@
         {#each tabs as tab, index}
             <a
                 href={tab.link}
-                class="duration-200 hover:text-violet-400"
+                class="duration-200 hover:text-blue-400"
                 target={index === 2 ? "_blank" : ""}
             >
                 <p>{tab.name}</p>
             </a>
         {/each}
-        <a href="#social-info" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+        <a href="#social-info" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-black text-white">
             <div
-                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
             />
             <h4 class="relative z-9">Get in touch</h4>
         </a>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -15,8 +15,8 @@
             link: "https://github.com/Kuswalt/Formalytics"
         },
         {
-            name: "Pokédex",
-            icon: "",
+            name: "FitnessGo",
+            icon: "fa-solid fa-dumbbell",
             description: "",
             link: ""
         },
@@ -25,16 +25,16 @@
     let benefits = [
         {
             metric: "10x",
-            name: "a self taught developer",
-            description: "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.",
+            name: "a self taught student developer",
+            description: "text",
         },
         {
-            name: "a product design & UX finatic",
-            description: "Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences.",
+            name: "an aspiring data scientist",
+            description: "text",
         },
         {
             name: "an excellent communicator",
-            description: "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team.",
+            description: "text",
         },
     ];
 
@@ -53,7 +53,7 @@
         {
             name: "Facebook",
             icon: "fa-brands fa-facebook",
-            link: "https://facebook.com/Chris-Jen-Roa",
+            // link: "https://www.facebook.com/chrisjen.roa",
             description: "Chris Jen Roa",
         },
     ];
@@ -68,43 +68,44 @@
             class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
         >
             <h5 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Hi! I'm <span class="poppins text-violet-400">Chris Jen Ian Roa</span>
+                Hi! I'm <span class="poppins text-blue-400">Chris Jen Ian D. Roa</span>
                 <br />A Developer
-                <span class="poppins text-violet-400">Student</span>
+                <span class="poppins text-blue-400">Student</span>
             </h5>
             <!-- <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400"> favorite tech</span> includes JavaScript
+                My <span class="text-blue-400"> favorite tech</span> includes JavaScript
                 (NEXT.JS or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL
                 or Firebase/Firestore!
             </p> -->
-            <a href="#social-info" class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
+            <a href="#social-info" class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-black text-white">
                 <div
-                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                    class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
                 />
                 <h4 class="relative z-9">Get in touch &rarr;</h4>
             </a>
         </div>
         <div class="relative shadow-2xl grid place-items-center">
             <img
-                src={"images/profileB.jpg"}
+                src={"images/profileB1.jpg"}
                 alt="img"
                 class="object-cover z-[2] max-h-[70vh] rounded-full"
             />
         </div>
     </section>
 
-
     <section
         id="about"
         class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
     >
-        <!-- <div class="z-[-1] bg-violet-950 w-screen left-1/2 -translate-x-1/2 top-0 h-full absolute"> </div> -->
+        <!-- <div class="z-[-1] bg-black w-screen left-1/2 -translate-x-1/2 top-0 h-full absolute"> </div> -->
         <div
-            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-blue-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-blue-700 py-4"
         >
-           
+        <h6 class="text-large sm:text-xl md:text-2xl">
+            Curious to know more?
+        </h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                 <span class="poppins text-violet-400">about</span> me.
+                 Here's a bit more <span class="poppins text-blue-400">about</span> me.
             </h3>
         </div>
         <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
@@ -130,72 +131,63 @@
         </div>
     </section>
 
-
-
     <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
         <div class="flex flex-col gap-2 text-center">
             <h6 class="text-large sm:text-xl md:text-2xl">
-                A few of my projects this past years.
+                A few of my projects this recent times.
             </h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Curious to <span class="poppins text-violet-400">see</span> my work?
+                Want to <span class="poppins text-blue-400">see</span> my works?
             </h3>
         </div>
 
-
-
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
             <Step step={steps[0]}>
-                  
                 <p>
                     WeatherWise is a webapp weather forecast with location detetction that utilizes
-                     <strong class="text-violet-400"> Angular Framework</strong>, <strong class="text-violet-400">RestApi PHP</strong> & <strong class="text-violet-400">OpenWeather API </strong>to show 5 hour, 5 day weather forecasts.
+                     <strong class="text-blue-400"> Angular Framework</strong>, <strong class="text-blue-400">RestApi PHP</strong> & <strong class="text-blue-400">OpenWeather API </strong>to show 5 hour, 5 day weather forecasts.
                 </p>
                 <div class="flex-1 flex justify-betweeen gap-4 items-end">
                     <div
-                        class="ml-auto cursor-pointer hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:bg-white after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
+                        class="ml-auto cursor-pointer hover:text-white duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:bg-black after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
                     >
-                        <a href={steps[0].link} class="text-violet-400">Go to &rarr;</a>
+                        <a href={steps[0].link} class="text-blue-400">Go to &rarr;</a>
                     </div>
                 </div>
             </Step>
-
 
             <Step step={steps[1]}>
                 <p>
                     Formalytics is a group project for Application Development Course that utilizes <strong
-                        class="text-violet-400">Angular Framework</strong
-                    > & <strong class="text-violet-400">RestApi PHP</strong>. A webapp
+                        class="text-blue-400">Angular Framework</strong
+                    > & <strong class="text-blue-400">RestApi PHP</strong>. A webapp
                     that allows a user to send questions to alumnis to know their currrent life state and do analytical results based on the survey response.
                 </p>
                 <div class="flex-1 flex justify-betweeen gap-4 items-end">
                     <div
-                        class="ml-auto cursor-pointer hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:bg-white after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
+                        class="ml-auto cursor-pointer hover:text-white duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:bg-black after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
                     >
-                        <a href={steps[1].link} class="text-violet-400">Go to &rarr;</a>
+                        <a href={steps[1].link} class="text-blue-400">Go to &rarr;</a>
                     </div>
                 </div>
             </Step>
 
-
-
-
             <Step step={steps[2]}>
                 <!-- <p>
-                    The Pokédex is a <strong class="text-violet-400"
+                    The Pokédex is a <strong class="text-blue-400"
                         >SvelteKit & TailwindCSS</strong
                     >
                     web application, hosted on
-                    <strong class="text-violet-400">Netlify</strong>, that
+                    <strong class="text-blue-400">Netlify</strong>, that
                     consumes and caches the
-                    <strong class="text-violet-400">Pokémon API</strong>
+                    <strong class="text-blue-400">Pokémon API</strong>
                     to display all Pokémon information. Gotta catch them all!
                 </p> -->
                 <div class="flex-1 flex justify-betweeen gap-4 items-end">
                     <div
-                        class="ml-auto cursor-pointer hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:bg-white after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
+                        class="ml-auto cursor-pointer hover:text-white duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:bg-black after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
                     >
-                        <a href={steps[2].link} class="text-violet-400">Go to &rarr;</a>
+                        <a href={steps[2].link} class="text-blue-400">Go to &rarr;</a>
                     </div>
                 </div>
             </Step>
@@ -208,7 +200,7 @@
                 Connect with me.
             </h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Follow me on <span class="poppins text-violet-400">social media</span>
+                Follow me on <span class="poppins text-blue-400">social media</span>
             </h3>
         </div>
 
@@ -216,7 +208,7 @@
             {#each socials as social}
             <Step step={social}>
                 {#if social.link}
-                <a href={social.link} class="text-violet-400 hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-full after:left-0 after:bg-white after:w-0 after:duration-200 hover:after:w-full after:z-[-1] overflow-hidden" target="_blank" rel="noopener noreferrer">{social.description}</a>
+                <a href={social.link} class="text-blue-400 hover:text-white duration-200 relative after:absolute after:top-0 after:h-full after:left-0 after:bg-black after:w-0 after:duration-200 hover:after:w-full after:z-[-1] overflow-hidden" target="_blank" rel="noopener noreferrer">{social.description}</a>
             {:else}
                     <p>{social.description}</p>
                 {/if}
@@ -226,4 +218,3 @@
     </section>
 
 </main>
-
